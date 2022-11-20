0e75bba26ad1678718ee161c6f88450898478630
Danny Garcia <ghpagesdannygarciacs@gmail.com>
2022-11-20 16:52:29 -0500

    async

--- js/chocobo.js
+++ js/chocobo.js
@@ -1,10 +1,17 @@
+// Event handling
+document.addEventListener("mousemove", animateTerrain);
+let mouseY = 0;
+let mouseX = 0;
+function animateTerrain(event) {
+    mouseY = event.clientY;
+    mouseX = event.clientX;
+}
 
 // Waits for css to load
 document.addEventListener("DOMContentLoaded", () => {
 
-    // 3D variables initialization
-    let chocobo;
-    var mixer;
+    let backdrop = null;
+    let chocobo = null;
 
     // Loads used textures
     const loader = new THREE.TextureLoader();
@@ -24,72 +31,73 @@ document.addEventListener("DOMContentLoaded", () => {
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setClearColor(0xffffff, 0);
 
-    // Loads GLTF scene
-    const gltfLoader = new THREE.GLTFLoader();
-    gltfLoader.load("model/scene.gltf", function(gltf){
+    // Loads models asynchronous
+    let promise = new Promise( function(resolve, reject) {
 
-        // Gets chocobo into the scene
-        scene.add(gltf.scene);
-        chocobo = gltf.scene.children[0];
-        chocobo.rotation.z = 2
+        try {
 
-        // Animates chocobo
-        mixer = new THREE.AnimationMixer(gltf.scene);
-        gltf.animations.forEach((clip) => {
-            mixer.clipAction(clip).play();
-        });
+            // Backdrop object
+            const backdropMaterial = new THREE.MeshBasicMaterial({ map, transparent: true });
+            backdrop = new THREE.Mesh(new THREE.PlaneGeometry(3, 3, 1, 1), backdropMaterial);
+            const backdropY = 1.0;
+            backdrop.rotation.x = 6.3;
+            backdrop.position.y = backdropY;
+            backdrop.position.z = -1;
+            scene.add(backdrop);
 
-        // Executes main loop after model loaded
-        animate();
+            // Loads GLTF scene
+            const gltfLoader = new THREE.GLTFLoader();
+            gltfLoader.load("model/scene.gltf", function(gltf){
+
+                // Gets chocobo into the scene
+                scene.add(gltf.scene);
+                chocobo = gltf.scene.children[0];
+                chocobo.rotation.z = 2
+
+                // Animates chocobo
+                const mixer = new THREE.AnimationMixer(gltf.scene);
+                gltf.animations.forEach((clip) => {
+                    mixer.clipAction(clip).play();
+                });
+
+                // Defines animation behavior
+                function animate() {
+
+                    // Moves backdrop based on mouse position
+                    let xTarget = 0;
+                    if(mouseX != 0) { xTarget = (-mouseX + (screen.width / 2)) / 6000 ; }
+                    else { xTarget = 0; }
+                    gsap.to(backdrop.position, {
+                        x: xTarget,
+                        y: backdropY + mouseY / 6000,
+                        duration: 1
+                    })
+
+                    // Moves chocobo based on mouse position
+                    gsap.to(chocobo.position, {
+                        x: -xTarget,
+                        y: -mouseY / 6000,
+                        duration: 1
+                    })
+
+                    // Moves chocobo's animation
+                    mixer.update(0.01);
+
+                    // Renders frame
+                    requestAnimationFrame(animate);
+                    renderer.render(scene, camera);
+                }
+
+                animate();
+            });
+        } catch(e) { reject(e); }
+
+        // Logs loading result
+        resolve("success");
     });
 
-    const backdropMaterial = new THREE.MeshBasicMaterial({
-    map, transparent: true
-    }); const backdrop = new THREE.Mesh(new THREE.PlaneGeometry(3, 3, 1, 1), backdropMaterial);
-    let backdropY = 1.0;
-    backdrop.rotation.x = 6.3;
-    backdrop.position.y = backdropY;
-    backdrop.position.z = -1;
-    scene.add(backdrop);
-
-    // Event handling
-    document.addEventListener("mousemove", animateTerrain);
-    let mouseY = 0;
-    let mouseX = 0;
-    function animateTerrain(event) {
-        mouseY = event.clientY;
-        mouseX = event.clientX;
-    }
-
-    // Main renderer loop
-    function animate() {
-
-        // Moves backdrop based on mouse position
-        let xTarget = 0;
-        if(mouseX != 0) { xTarget = (-mouseX + (screen.width / 2)) / 6000 ; }
-        else { xTarget = 0; }
-        gsap.to(backdrop.position, {
-            x: xTarget,
-            y: backdropY + mouseY / 6000,
-            duration: 1
-        })
-
-        // Rotates chocobo
-        chocobo.rotation.z -= 0.0005;
-
-        // Moves chocobo based on mouse position
-        gsap.to(chocobo.position, {
-            x: -xTarget,
-            y: -mouseY / 6000,
-            duration: 1
-        })
-
-        // Moves chocobo's animation
-        mixer.update(0.01);
-
-        // Renders frame
-        requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-
-    }
+    promise.then(
+        function(result) { console.log(result); },
+        function(error) { console.log(error); }
+    );
 });
